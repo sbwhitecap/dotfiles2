f5080a85beffc723cdb1b8b83a246acd28c7f102
Kazuki Shigemichi <shigemichik@gmail.com>
2020-05-12 16:56:03 +0900

    ccl: ストリームやソケットを取り扱う際の既定の文字符号化方式をUTF-8に設定する。

--- ccl-init.lisp
+++ ccl-init.lisp
@@ -1,3 +1,8 @@
+(setf ccl:*default-external-format*
+            (ccl:make-external-format :character-encoding :utf-8
+                                              :line-termination :unix)
+            ccl:*default-file-character-encoding* :utf-8
+            ccl:*default-socket-character-encoding* :utf-8)
 
 ;;; The following lines added by ql:add-to-init-file:
 #-quicklisp
